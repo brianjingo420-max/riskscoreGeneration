bd5390428b896ea41dba8058cc86bee7f32818b8
brianjingo420-max <brianjingo420@gmail.com>
2026-08-28 10:28:56 +0900

    Update 01. RNA-seq transcript quantification using Salmon (Training cohort).sh

--- scripts/01. RNA-seq transcript quantification using Salmon (Training cohort).sh
+++ scripts/01. RNA-seq transcript quantification using Salmon (Training cohort).sh
@@ -1,1 +1,20 @@
+# Prepare Salmon indexing
+Ensembl release 114
+FASTA=Canis_lupus_familiaris.ROS_Cfam_1.0.cdna.all.fa
 
+# Session
+Salmon v1.10.2
+
+salmon index -t "{$FASTA}" -i "{$INDEX_PATH}" -k 31
+
+# Salmon quantification
+salmon quant \
+  -i "${INDEX_PATH}" \
+  -l A \
+  -1 "sample01_R1.fastq" \
+  -2 "sample01_R2.fastq" \
+  -p 8 \
+  --validateMappings \
+  -o "${OUTPUT_DIR}"
+
+  
